eff8ad070401e2910afe9d3d08eee0b56b47d37f
AhmedReda958 <asateralcomedi123@gmail.com>
2023-02-13 15:35:29 +0200

    sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -74,7 +74,7 @@ self.addEventListener("message", (event) => {
 
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
+
 const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
 const OFFLINE_URL = "offline.html";
