2a5d73433ecda4cd4051b42f248ec13384ce42ae
AhmedReda958 <asateralcomedi123@gmail.com>
2023-02-13 15:30:20 +0200

    pwa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "bootstrap";
 import App from "./App";
 import AOS from "aos";
@@ -11,3 +12,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
